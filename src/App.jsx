c87413fdd0e0d7aefcfc9c6eb3a4c8e7abfce0fb
Sasidhar Pinjala <sasidhar.pi@tmachinesoftware.com>
2025-05-22 15:34:17 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import './App.css'
+import Inbox from './components/Inbox'
+
+function App() {
+
+  return (
+    <div className="content-wrapper">
+
+      <div className="messages-div">
+        <Inbox />
+      </div>
+
+
+      <div className="messages-description-div">2</div>
+
+
+      <div className="copilot-div">3</div>
+
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Inbox.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Inbox = () => {
+  return (
+
+    <>
+        <div className='inbox'>
+
+            <div className='inbox-heading'>
+                Your inbox
+            </div>
+            
+            <div className='messages'>
+              
+            </div>
+
+        </div>
+
+        <div className='inbox-footer'>
+
+        </div>
+    </>
+  )
+}
+
+export default Inbox
